4f9750e42bcad4ae6937df673c40022c8c38c546
Nipun Chhajed <nipunchhajed@gmail.com>
2016-04-27 20:29:49 -0700

    This is my first commit to bitbucket.

---
+++ QuickNotes/NoteManager.swift
@@ -0,0 +1,13 @@
+//
+//  NoteManager.swift
+//  QuickNotes
+//
+//  Created by Nipun Chhajed on 4/27/16.
+//  Copyright © 2016 Program5Studios. All rights reserved.
+//
+
+import UIKit
+
+class NoteManager: NSObject {
+
+}
